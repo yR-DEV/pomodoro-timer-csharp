ed81aa742ad6fd727da98730b288f1b29f026ee8
yeah-right <claar.zack@gmail.com>
2019-05-18 11:31:40 -0600

    Initial commit, program building and running without errors

---
+++ Pomodoro/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace Pomodoro
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
